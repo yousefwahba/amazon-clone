8a01d7890a45cebc7366642fff291045412559c9
yousefwahba <yousefwahba.official@gmail.com>
2022-08-09 07:17:53 +0200

    add layout

---
+++ components/Layout.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + '- Amazon' : 'Amazon'}</title>
+        <meta name="description" content="Amazon clone " />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex justify-between h-12 items-center px-4 shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">Amazon</a>
+            </Link>
+            <div className="links">
+              <Link href="/card">
+                <a className="p-2">Card</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="bg-blue-400 container m-auto mt-4 px-4">
+          {children}
+        </main>
+        <footer className="flex justify-center items-center h-12 shadow-inner capitalize">
+          copyright amazon
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
