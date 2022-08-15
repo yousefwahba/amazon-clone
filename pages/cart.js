2fc265363b9b62f12a4e5994a625509080eb79d0
yousefwahba <yousefwahba.official@gmail.com>
2022-08-15 10:37:24 +0200

    create login page with validations

--- pages/cart.js
+++ pages/cart.js
@@ -93,7 +93,7 @@ const cart = () => {
               <li>
                 <button
                   className="primary-button w-full"
-                  onClick={() => router.push('/shipping')}
+                  onClick={() => router.push('login?redirect=/shipping')}
                 >
                   Check Out
                 </button>
