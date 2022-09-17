1491954ad3a9d3a3c239fc3d53663d4e90ef4cb8
yousefwahba <yousefwahba.official@gmail.com>
2022-09-17 12:58:08 +0200

    insert products in database

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,4 @@
+import Product from '../../models/Product';
 import User from '../../models/User';
 import data from '../../utils/data';
 import db from '../../utils/db';
@@ -5,6 +6,8 @@ const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ massage: 'seeded successfully' });
 };
