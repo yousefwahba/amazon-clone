1429e9d85a492033ce95fc4f475b4c1a5c212427
yousefwahba <yousefwahba.official@gmail.com>
2022-09-20 10:35:32 +0200

    fix build bug

--- pages/payment.js
+++ pages/payment.js
@@ -1,17 +1,20 @@
-import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import Cookies from 'js-cookie';
 import CheckoutWizard from '../components/CheckoutWizard';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 
-function Payment() {
+export default function PaymentScreen() {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const { shippingAddress, paymentMethod } = cart;
+
   const router = useRouter();
+
   const submitHandler = (e) => {
     e.preventDefault();
     if (!selectedPaymentMethod) {
@@ -25,6 +28,7 @@ function Payment() {
         paymentMethod: selectedPaymentMethod,
       })
     );
+
     router.push('/placeorder');
   };
   useEffect(() => {
@@ -39,16 +43,17 @@ function Payment() {
       <CheckoutWizard activeStep={2} />
       <form className="mx-auto max-w-screen-md" onSubmit={submitHandler}>
         <h1 className="mb-4 text-xl">Payment Method</h1>
-        {['Paypal', 'Stripe', 'CashOnDelivery'].map((payment) => (
-          <div className="mb-4" key={payment}>
+        {['PayPal', 'Stripe', 'CashOnDelivery'].map((payment) => (
+          <div key={payment} className="mb-4">
             <input
               name="paymentMethod"
               className="p-2 outline-none focus:ring-0"
               id={payment}
               type="radio"
-              checked={selectedPaymentMethod == payment}
+              checked={selectedPaymentMethod === payment}
               onChange={() => setSelectedPaymentMethod(payment)}
             />
+
             <label className="p-2" htmlFor={payment}>
               {payment}
             </label>
@@ -56,9 +61,7 @@ function Payment() {
         ))}
         <div className="mb-4 flex justify-between">
           <button
-            onClick={() => {
-              router.push('/shipping');
-            }}
+            onClick={() => router.push('/shipping')}
             type="button"
             className="default-button"
           >
@@ -71,5 +74,4 @@ function Payment() {
   );
 }
 
-export default Payment;
-Payment.aut = true;
+PaymentScreen.auth = true;
